6937d074e633bef4456454a160e856f026604871
leandro-driguez <leandro_driguez@outlook.com>
2023-10-17 12:06:19 -0400

    fix: change the humorous death texts from metawars_api.py to the internal_data.json file that is loaded

--- src/metawars_api.py
+++ src/metawars_api.py
@@ -15,70 +15,14 @@ DATA = load_internal_data()
 UNITS_DATA = DATA["units"]
 WEAPON_DATA = DATA["weapon"]
 ARMOUR_DATA = DATA["armour"]
-NAMES_DATA=DATA["names"]
+NAMES_DATA = DATA["names"]
+DEATHS_DATA = DATA["deaths"]
 
 UNITY_TYPES = list(UNITS_DATA.keys())
 WEAPON_TYPES = list(list(WEAPON_DATA.values())[0].keys())
 ARMOUR_TYPES = list(list(ARMOUR_DATA.values())[0].keys())
 UNIT_NAMES = list(NAMES_DATA.values())
-
-DEATHS = [ "was cut in two pieces", "tripped on their own sword and impaled themselves", "mistook a dragon for a friendly ally", "got lost in their own castle and starved to death", "tried to cast a fireball spell but set themselves on fire instead", "ate a poisonous mushroom thinking it was a magical potion", "challenged a troll to a tickling contest and was squashed", "accidentally stepped into a teleportation portal and ended up in the enemy's camp", "tried to ride a unicorn but got kicked into oblivion", "fell into a pit trap filled with rabid bunnies", "decided to juggle live grenades and failed spectacularly", "choked on a turkey leg during a victory feast", "slipped on a banana peel and fell into a moat filled with crocodiles", "insulted a powerful wizard who turned them into a chicken", "tried to bribe the enemy with counterfeit gold and was promptly executed", "thought they were invisible after drinking an invisibility potion, but they weren't", "decided to take a nap on the battlefield and was mistaken for a fallen soldier", "tried to negotiate a peace treaty with a horde of bloodthirsty goblins", "accidentally triggered their own booby trap", "got tangled in their own chainmail and couldn't escape", "ate a cursed apple and fell into an eternal slumber", "thought they were invincible after drinking a potion of invulnerability, but it was just grape juice", "tried to impress their crush by jousting blindfolded and impaled themselves on a tree", "fell into a pit of quicksand and sank while reciting a heroic monologue", "tried to tame a wild griffin but ended up as its lunch", "danced too vigorously during a victory celebration and fell off a cliff", "attempted to behead a zombie but their sword got stuck in its neck", "tried to cast a lightning spell during a thunderstorm and was struck by real lightning", "fell asleep on watch duty and woke up surrounded by enemy soldiers", "tried to ride a wyvern but accidentally pulled its tail and got incinerated", "ate a cursed pie and turned into a talking squirrel", "thought they could fly after drinking a levitation potion and jumped off a tower", "got entangled in their own catapult and launched themselves into the enemy lines", "tried to use a magic wand but accidentally turned themselves into a toad", "slipped on a banana peel and fell into a pit of hungry wolves", "tried to play dead to trick the enemy but ended up being buried alive", "thought they could breathe underwater and drowned in a shallow pond", "tried to tame a ferocious warhorse but got dragged across the battlefield instead", "tripped over their own beard and fell off a cliff", "attempted to use a cursed sword and was instantly turned into a statue", "tried to use a cursed mirror and got trapped inside it forever", "thought they could communicate with animals and was devoured by a pack of wolves", "tried to sneak into the enemy camp disguised as a sheep but was mistaken for dinner", "got lost in a maze of their own design and starved to death", "thought they could control the weather and summoned a lightning storm that struck them down", "tried to impress their comrades with a daring acrobatic maneuver but ended up impaled on a flagpole", "ate a magical mushroom and grew so tall that they couldn't fit through the castle gates", "tried to use a cursed amulet and was cursed with eternal bad luck", "thought they could outrun a charging rhinoceros but ended up as a pancake", "tried to capture a basilisk but was turned to stone by its deadly gaze", "attempted to tame a wild dragon but ended up as its snack", "thought they could communicate with trees and was crushed by a falling oak", "got their foot stuck in a bear trap and couldn't escape", "tried to use a cursed compass and ended up wandering in circles for eternity", "thought they could outdrink a group of dwarves and succumbed to alcohol poisoning", "tried to swim across a river infested with piranhas", "thought they could outsmart a group of cunning goblins but fell into their own trap", "got their head stuck in a suit of armor and suffocated", "tried to tame a wild centaur but got trampled instead", "thought they could outdance a group of enchanted skeletons but ended up as a pile of bones", "ate a cursed sandwich and turned into a talking donkey", "tried to use a cursed ring and became invisible forever", "thought they could outwit a group of witches but ended up as a frog", "got their beard caught in a catapult and was launched into the enemy lines", "tried to tame a pack of wild wolves but became their dinner instead", "thought they could outmaneuver a group of skilled archers but ended up as a pincushion", "ate a cursed chocolate and turned into a giant marshmallow", "tried to use a cursed book and was sucked into its pages", "thought they could outsmart a group of mischievous fairies but ended up as their plaything", "got their foot stuck in a giant Venus flytrap and became its meal", "tried to tame a wild minotaur but got trampled by its hooves", "thought they could outswim a group of mermaids but drowned in the process", "ate a cursed cookie and turned into a living gingerbread man", "tried to use a cursed staff and was transformed into a statue", "thought they could outwit a group of clever trolls but ended up as their bridge", "got their arm stuck in a dragon's hoard and was burned to a crisp", "tried to tame a swarm of angry bees but got stung to death", "thought they could outplay a group of mischievous imps but ended up as their eternal entertainment", "ate a cursed candy and turned into a talking skeleton", "tried to use a cursed potion and was transformed into a giant slug", "thought they could outrun a group of enchanted forest animals but got trampled by a stampede", "got their hand stuck in a giant venus flytrap and was slowly digested", "tried to tame a wild chimera but got devoured by its fiery breath", "thought they could outsmart a group of clever sphinxes but failed their riddles and met a gruesome fate", "ate a cursed cupcake and turned into a living statue", "tried to use a cursed scroll and was transported to a realm of eternal darkness", "thought they could outmaneuver a group of cunning harpies but ended up as their meal", "got their foot stuck in a giant clam and drowned as the tide came in", "tried to tame a pack of wild hyenas but became their prey instead", "thought they could outwit a group of trickster leprechauns but ended up as their servant", "ate a cursed ice cream and turned into a frozen statue", "tried to use a cursed talisman and was trapped in a never-ending nightmare", "thought they could outplay a group of mischievous pixies but ended up as their plaything", "got their head stuck in a giant pitcher plant and was slowly dissolved", "tried to tame a wild basilisk but was turned to stone by its deadly gaze", "thought they could outsmart a group of cunning sirens but drowned in their enchanting song", "ate a cursed donut and turned into a living jelly", "tried to use a cursed artifact and was banished to a realm of eternal torment", "thought they could outrun a group of lightning-fast sprites but was zapped into oblivion", "got their foot stuck in quicksand and sank to their demise", "tried to tame a wild manticore but got impaled by its poisonous tail", "thought they could outwit a group of clever gnomes but ended up as their garden ornament", "ate a cursed pizza and turned into a living statue", "tried to use a cursed relic and was trapped in a dimension of eternal chaos", "thought they could outmaneuver a group of cunning gremlins but ended up as their mechanical plaything", "got their head stuck in a giant pitcher plant and suffocated", "tried to tame a pack of wild jackals but became their prey instead", "thought they could outsmart a group of clever kobolds but ended up as their prisoner", "ate a cursed burger and turned into a living slime", "tried to use a cursed totem and was cursed with it", "was tickled to death by enemy soldiers", "was poked to death with a foam sword", "died from laughter after being insulted by the enemy", "was defeated by a deadly game of rock-paper-scissors", "was smothered to death by a barrage of feather pillows", "was defeated by a fierce tickle fight", "tripped over their own shoelaces and was trampled by the enemy", "was defeated in a thumb war", "was scared to death by the enemy's silly faces", "was defeated by a group of enemy soldiers armed with rubber chickens", "was tickled by a feather until they couldn't breathe", "was defeated by a surprise attack of enemy soldiers armed with rubber ducks", "was defeated in a game of medieval charades and died of embarrassment", "was defeated by an enemy who couldn't stop telling bad jokes", "was defeated by a group of enemy soldiers armed with whoopee cushions", "was outsmarted by the enemy in a game of medieval trivia", "was defeated by an enemy who used a rubber chicken as a weapon", "was defeated in a game of medieval hopscotch and fell into a pit of doom", "was defeated by a group of enemy soldiers armed with silly string", "was defeated by an enemy who had an unstoppable dance-off", "was defeated in a game of medieval musical chairs and was left behind", "was defeated by an enemy who used a banana peel as a weapon", "was defeated in a game of medieval limbo and couldn't get back up", "was defeated by a group of enemy soldiers armed with water balloons", "was defeated by an enemy who had an unbeatable game of medieval charades", "was defeated in a game of medieval Simon Says and couldn't stop", "was defeated by an enemy who used a whoopee cushion as a weapon", "was defeated in a game of medieval hide-and-seek and was never found", "was defeated by a group of enemy soldiers armed with rubber spiders", "was defeated by an enemy who had an unstoppable game of medieval tag", "was defeated in a game of medieval musical chairs and was left without a seat", "was defeated by an enemy who used a rubber chicken as a boomerang", "was defeated in a game of medieval hopscotch and stepped on the wrong square", "was defeated by a group of enemy soldiers armed with water guns", "was defeated by an enemy who had an unbeatable game of medieval trivia", "was defeated by an enemy who used a banana peel as a trap", "was defeated by an enemy who had an unbeatable game of medieval tag", "was defeated by an enemy who had an unstoppable game of medieval trivia", "was tickled to death by an enemy knight",
-"was defeated by a deadly staring contest",
-"was smothered to death by a rain of cotton candy",
-"was trampled by their own army during a dance-off",
-"was defeated by a horde of angry bunnies",
-"died from a paper cut inflicted by a love letter",
-"was mistaken for a scarecrow and pecked to death by crows",
-"was defeated by a group of knights armed with rubber chickens",
-"was crushed under the weight of their own armor while sneezing",
-"was outsmarted by a cunning enemy squirrel",
-"died from a heart attack caused by a surprise birthday party",
-"was defeated by a deadly game of thumb wars",
-"was suffocated by an enemy wizard's never-ending fart spell",
-"was accidentally swallowed by a dragon while trying to slay it",
-"was defeated by a troop of enemy clowns armed with water balloons",
-"was struck by lightning while trying to take a selfie during a storm",
-"was defeated by a relentless army of killer kittens",
-"died from laughter after hearing a particularly bad joke from the enemy",
-"was hypnotized by an enemy magician and danced themselves to death",
-"was defeated by a troop of enemy knights armed with whoopee cushions",
-"was crushed by a falling piano dropped by an enemy catapult",
-"was defeated in a medieval rap battle and died of embarrassment",
-"died from a severe allergic reaction to flowers thrown by admirers",
-"was defeated by an enemy with a deadly weaponized feather duster",
-"was accidentally swallowed by a giant whale during a naval battle",
-"was defeated by a group of enemy jesters armed with exploding pies",
-"died from a severe case of food poisoning after eating enemy rations",
-"was defeated by an army of enemy musicians armed with bagpipes",
-"was turned into a frog by an enemy sorcerer and couldn't change back",
-"was defeated by an enemy knight armed with a rubber chicken",
-"was accidentally catapulted into the enemy camp and mistaken for a spy",
-"was defeated by an enemy archer with a deadly barrage of marshmallows",
-"died from a severe case of helmet hair that caused a fatal distraction",
-"was defeated by a troop of enemy jesters armed with exploding confetti",
-"was trampled to death by a stampede of enemy unicorns",
-"was defeated by an enemy sorcerer who turned their sword into a banana",
-"was crushed by a giant rolling cheese wheel during a siege",
-"died from a fatal allergic reaction to their own suit of armor",
-"was defeated by a group of enemy knights armed with rubber chickens",
-"was accidentally swallowed by a dragon while trying to negotiate peace",
-"was defeated by an army of enemy musicians armed with kazoos",
-"was struck by a flying cow launched by an enemy trebuchet",
-"was defeated by an enemy knight who tickled them into submission",
-"died from a fatal case of stage fright during a battle speech",
-"was defeated by an enemy with a deadly weaponized feather boa",
-"was crushed under the weight of their own armor while attempting a cartwheel",
-"was defeated by a group of enemy archers armed with rubber bands",
-"died from an unfortunate encounter with a cursed fortune cookie",
-"was defeated by an enemy sorcerer who turned their horse into a frog",
-"was accidentally swallowed by a giant snake during a reconnaissance mission",
-"was defeated by an army of enemy jesters armed with exploding balloons",
-"died from an extreme case of sunburn after forgetting to wear sunscreen",
-"was defeated by an enemy knight who challenged them to a dance-off",
-"was crushed by a falling anvil dropped by an enemy airship",
-"was defeated by a troop of enemy musicians armed with accordions",
-"died from a severe case of food poisoning after eating enemy jester pies",
-"was defeated by an enemy knight who defeated them in a game of chess"]
+DEATHS = list(DEATHS_DATA.values())
 
 class Weapon:
     wood = "wood"
